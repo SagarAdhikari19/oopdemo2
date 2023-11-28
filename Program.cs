e38d64e8337a449e72bf2b0fd063f7d1ffb8318c
sagar adhikari <sagarjungadhikari19@gmail.com>
2023-11-28 10:42:05 +0545

    updated main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// // See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
+class EProgram{
+
+    public static void Main(string[] args){
+        Employee sagar = new Employee(1,"Sagar Adhikari",400000);
+        sagar.salarycal();
+        Console.ReadLine();
+
+
+    }
+}
