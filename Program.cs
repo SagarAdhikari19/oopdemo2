7e9af161d86c7a8b6ade58e3b849d4d4f45f4d5b
sagar adhikari <sagarjungadhikari19@gmail.com>
2023-11-28 12:19:14 +0545

    ready to push

--- Program.cs
+++ Program.cs
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-﻿// // See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-class EProgram{
-
-    public static void Main(string[] args){
-        Employee sagar = new Employee(1,"Sagar Adhikari",400000);
-        sagar.salarycal();
-        Console.ReadLine();
-=======
 ﻿using ABSTRACT_CLASS;
 
 class EProgram{
@@ -23,7 +13,6 @@ class EProgram{
         sagar.PhoneNumber="9808850889";
         sagar.rollnum=9;
         
->>>>>>> abstractclass
 
 
     }
